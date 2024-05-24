edd28d0b2ddb4115bce8e13abec65036251a45ab
iqblfrdsyh <iqblfrdsyh@gmail.com>
2024-05-24 22:10:05 +0700

    adding icon metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,17 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export const metadata = {
   title: "Joy AI",
   description: "Discover the Joy of AI: Empowering Tomorrow, Today.",
+  icons: {
+    icon: [
+      "/images/icons/joy-ai-icon.png"
+    ],
+    apple: [
+      "/images/icons/joy-ai-icon.png"
+    ],
+    shortcut: [
+      "/images/icons/joy-ai-icon.png"
+    ]
+  }
 };
 
 export default function RootLayout({ children }) {
